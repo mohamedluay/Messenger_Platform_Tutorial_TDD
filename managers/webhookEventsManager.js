5733ee850a9f5e9e3c50279cb0ea05c134816fdf
Mohamed Luay <mohamed.luay89@gmail.com>
2020-11-30 23:36:46 +0200

    [R]- lesson 6 code

--- managers/webhookEventsManager.js
+++ managers/webhookEventsManager.js
@@ -16,19 +16,6 @@ const extractCommonAttributes = (event) => {
 const parseMessageEvent = (event) => {
     // ToDo: Lesson 3_1
     // ToDo: Lesson_6
-    return {
-        eventType: WEBHOOK_EVENT_TYPES.MESSAGE,
-        userPSID: event.sender.id,
-        userReference: event.sender.user_ref,
-        sendingPageID: event.recipient.id,
-        timestamp: event.timestamp,
-        isQuickReply: event.message.quick_reply != undefined,
-        isReplyToPreviousMessage: event.message.reply_to != undefined,
-        hasAttachments: event.message.attachments != undefined,
-        message: event.message,
-        entities: extractEntitiesFromMessageObject(event.message),
-        traits: extractTraitsFromMessageObject(event.message),
-    };
 };
 
 const parsePostbackEvent = (event) => {
@@ -41,26 +28,10 @@ const parseReferralEvent = (event) => {
 
 const extractEntitiesFromMessageObject = (message) => {
     // ToDo: Lesson_6
-    if (!message.entities) return [];
-    const entitiesNames = Object.keys(message.entities);
-    const entities = entitiesNames.map((entityName) => ({
-        name: entityName,
-        confidence: message.entities[entityName][0].confidence,
-        value: message.entities[entityName][0].value,
-    }));
-    return entities;
 };
 
 const extractTraitsFromMessageObject = (message) => {
     // ToDo: Lesson_6
-    if (!message.traits) return [];
-    const traitNames = Object.keys(message.traits);
-    const traits = traitNames.map((traitName) => ({
-        name: traitName,
-        confidence: message.traits[traitName][0].confidence,
-        value: message.traits[traitName][0].value,
-    }));
-    return traits;
 };
 
 module.exports = {
